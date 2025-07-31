161f4f644e130d0c858f9d7895b4ac315894c239
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2025-07-30 19:28:10 -0600

    fix: update experience validation schema to ensure required fields are properly defined

--- src/pages/admin/experiences/schema.ts
+++ src/pages/admin/experiences/schema.ts
@@ -4,27 +4,53 @@ import * as yup from 'yup'
 export const createExperienceSchema = yup
   .object({
     title: yup.string().required('Experience title is required'),
+    description: yup.string().required('Description is required'),
+    userId: yup.number().required('Pick a valid user'),
     company: yup.string().required('Company is required'),
     startDate: yup.string().required('Start date is required'),
-    endDate: yup.string().nullable().optional(),
-    description: yup.string().required('Description is required'),
-    userId: yup.string().required('Pick a valid user'),
+    endDate: yup
+      .string()
+      .nullable()
+      .transform((value, originalValue) => {
+        return originalValue === '' ? undefined : value
+      })
+      .default(null),
     employmentType: yup.string().required('Employment type is required'),
     location: yup.string().required('Location is required'),
-    companyWebsite: yup.string().url('Invalid URL').nullable().optional(),
+    companyWebsite: yup
+      .string()
+      .url('Invalid URL')
+      .nullable()
+      .transform((value, originalValue) => {
+        return originalValue === '' ? null : value
+      })
+      .default(null),
   })
   .required()
 
 export const editExperienceSchema = yup
   .object({
     title: yup.string().required('Experience title is required'),
+    description: yup.string().required('Description is required'),
+    userId: yup.number().required('Pick a valid user'),
     company: yup.string().required('Company is required'),
     startDate: yup.string().required('Start date is required'),
-    endDate: yup.string().nullable().optional(),
-    description: yup.string().required('Description is required'),
-    userId: yup.string().required('Pick a valid user'),
+    endDate: yup
+      .string()
+      .nullable()
+      .transform((value, originalValue) => {
+        return originalValue === '' ? null : value
+      })
+      .default(null),
     employmentType: yup.string().required('Employment type is required'),
     location: yup.string().required('Location is required'),
-    companyWebsite: yup.string().url('Invalid URL').nullable().optional(),
+    companyWebsite: yup
+      .string()
+      .url('Invalid URL')
+      .nullable()
+      .transform((value, originalValue) => {
+        return originalValue === '' ? null : value
+      })
+      .default(null),
   })
   .required()
