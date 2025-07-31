de9ae5c67bddfd868557d17afc6569727b7a3bd3
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2025-07-31 16:34:34 -0600

    feat: tooltip

--- src/components/home/technologies.tsx
+++ src/components/home/technologies.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { fetchTechnologiesByPage } from '../../store/slices/technologies.slice'
@@ -27,29 +27,52 @@ const Technologies = () => {
 
   return (
     <div className="mt-4 flex flex-wrap gap-4 justify-between">
-      {technologies.map(({ name, icon, tags }: ITechnology) => (
-        <div
-          key={`technology-${name}`}
-          className="flex flex-col gap-2 items-center border dark:border-white/10 dark:bg-white/5 border-black/10 bg-black/5 text-md p-3 md:rounded-sm rounded-full 
-      hover:bg-white/10 md:w-[20%]"
-        >
-          <img src={icon} alt={`${name} icon`} className="size-5 md:size-11" />
-          <span className="hidden md:block text-sm">{name}</span>
-          <div className="hidden md:flex items-center justify-center gap-2 flex-wrap">
-            {tags.map(({ name, color }: ITag) => {
-              return (
-                <Badge
-                  key={`${name}-${color}`}
-                  variant="filled"
-                  text={name.toLowerCase()}
-                  color={color}
-                  size="xs"
-                />
-              )
-            })}
-          </div>
-        </div>
+      {technologies.map((technology: ITechnology) => (
+        <TechnologyItem key={`technology-${technology.name}`} {...technology} />
       ))}
+    </div>
+  )
+}
+
+function TechnologyItem({
+  name,
+  icon,
+  tags,
+}: {
+  name: string
+  icon: string
+  tags: ITag[]
+}) {
+  const [isFocused, setIsFocused] = useState<boolean>(false)
+
+  return (
+    <div
+      tabIndex={0}
+      onFocus={() => setIsFocused(true)}
+      onBlur={() => setIsFocused(false)}
+      className="flex flex-col gap-2 items-center border dark:border-white/10 dark:bg-white/5 border-black/10 bg-black/5 text-md p-3 md:rounded-sm rounded-full 
+      hover:bg-white/10 md:w-[20%] relative"
+    >
+      <img src={icon} alt={`${name} icon`} className="size-5 md:size-11" />
+      <span className="hidden md:block text-sm">{name}</span>
+      {isFocused && (
+        <div className="md:hidden absolute top-10 p-1 text-xs text-center text-white bg-gray-400 rounded-md dark:bg-gray-700">
+          {name}
+        </div>
+      )}
+      <div className="hidden md:flex items-center justify-center gap-2 flex-wrap">
+        {tags.map(({ name, color }: ITag) => {
+          return (
+            <Badge
+              key={`${name}-${color}`}
+              variant="filled"
+              text={name.toLowerCase()}
+              color={color}
+              size="xs"
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
